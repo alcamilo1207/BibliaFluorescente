c7ab716bb4520fa4ad0c65ca6b12822dc82232d3
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-11-02 11:31:13 -0500

    full bible

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ def hello():
                   "2 Samuel","1 Reyes","2 Reyes","1 Crónicas","2 Crónicas","Esdrás","Nehemías","Ester",
                   "Job","Salmos","Proverbios","Eclesiastés","Cantares","Isaías","Jeremías","Lamentaciones",
                   "Ezequiel","Daniel","Oseas","Joel","Amós","Abdías","Jonás","Miqueas","Nahum", "Habacuc","Sofonías",
-                  "Hageo", "Zacarías", "Malaquías"]
+                  "Hageo", "Zacarías", "Malaquías","Mateo", "Marcos", "Lucas", "Juan", "Hechos", "Romanos", "1 Corintios", "2 Corintios",
+              "Gálatas","Efesios", "Filipenses", "Colosenses","1 Tesaloniscences", "2 Tesaloniscences","1 Timoteo", "2 Timoteo", "Tito",
+              "Filemón", "Hebreos", "Santiago", "1 Pedro", "2 Pedro", "1 Juan", "2 Juan", "3 Juan", "Judas", "Apocalipsis"]
 
 
 
